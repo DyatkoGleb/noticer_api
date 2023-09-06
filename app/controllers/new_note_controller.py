c3f9caee491463e05fdf1f0483423fa950f3760d
DyatkoGleb <glebikon@yandex.ru>
2023-09-06 14:18:40 +0300

    Move more logic to the controller

--- app/controllers/new_note_controller.py
+++ app/controllers/new_note_controller.py
@@ -2,12 +2,26 @@ import sys
 from constants import TYPE_NOTICE
 from models.note import Note
 from models.notice import Notice
+from services.note_message_parser_service import NoteMessageParserService
+from response.error_response import ErrorResponse
+from response.success_response import SuccessResponse
 
 sys.path.append('../')
 
 
 class NewNoteController:
-    def save_item_action(self, entity: dict) -> dict:
+    def save_note_action(self, note: dict) -> SuccessResponse | ErrorResponse:
+        entity = NoteMessageParserService().get_entity(note)
+
+        if 'error' in entity:
+            return ErrorResponse(entity['error']).get()
+
+        try:
+            return SuccessResponse(NewNoteController().save(entity))
+        except Exception as e:
+            return ErrorResponse(str(e)).get()
+
+    def save(self, entity: dict):
         item_type = entity['item_type']
 
         del entity['item_type']
--- app/router.py
+++ app/router.py
@@ -1,18 +1,9 @@
 from fastapi import APIRouter
-from services.note_message_parser_service import NoteMessageParserService
-from response.error_response import ErrorResponse
-from response.success_response import SuccessResponse
 from controllers.new_note_controller import NewNoteController
 
 router = APIRouter()
 
 
 @router.post("/addNewNote")
-async def add_new_note(item: dict):
-    entity = NoteMessageParserService().get_entity(item)
-
-    if 'error' in entity:
-        return ErrorResponse(entity['error']).get()
-
-    return SuccessResponse(NewNoteController().save_item_action(entity))
-
+async def add_new_note(note: dict):
+    return NewNoteController().save_note_action(note)
