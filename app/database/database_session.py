32ee0389351ad9ebac26c42deacb363723a433c1
DyatkoGleb <glebikon@yandex.ru>
2023-10-29 21:30:03 +0300

    Feat: Create DB if not exist

--- app/database/database_session.py
+++ app/database/database_session.py
@@ -1,5 +1,5 @@
 import os
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, exc
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 
@@ -14,8 +14,18 @@ class DatabaseSession:
         if self._instance is None:
             self._instance = super(DatabaseSession, self).__new__(self)
             self._instance.engine = create_engine(self.database_url, pool_size=20)
+
+            try:
+                self._instance.engine.connect()
+            except exc.OperationalError:
+                self.create_database()
+
             self._instance.Session = sessionmaker(bind=self._instance.engine)
         return self._instance
+
+    def create_database(self):
+        from sqlalchemy_utils import create_database
+        create_database(self.database_url)
 
     def get_session(self):
         return self.Session()
