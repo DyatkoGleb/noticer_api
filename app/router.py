e764c3cb53174b6af8a259ce082c7f691727317a
DyatkoGleb <glebikon@yandex.ru>
2024-01-03 20:25:45 +0300

    Refactor: add /api/ to paths

--- app/router.py
+++ app/router.py
@@ -5,38 +5,38 @@ from models import Note, Todo, Notice
 router = APIRouter()
 
 
-@router.post("/addNewNote")
+@router.post("/api/addNewNote")
 def add_new_note(note: dict):
     return NoteController().save_note_action(note)
 
-@router.get("/getNotes")
+@router.get("/api/getNotes")
 def get_notes(request: Request):
     return NoteController().get_notes_action(request)
 
-@router.post("/editNote")
+@router.post("/api/editNote")
 def edit_note(note: dict):
     return NoteController().save_updated_note_action(note)
 
-@router.post("/deleteNote")
+@router.post("/api/deleteNote")
 async def delete_note(note: dict):
     return NoteController().delete_note_action(Note, note)
 
-@router.get("/getTodos")
+@router.get("/api/getTodos")
 def get_todos(request: Request):
     return NoteController().get_todos_action(request)
 
-@router.post("/deleteTodo")
+@router.post("/api/deleteTodo")
 async def delete_todo(todo: dict):
     return NoteController().delete_note_action(Todo, todo)
 
-@router.get("/getAllNotices")
+@router.get("/api/getAllNotices")
 def get_all_notices(request: Request):
     return NoteController().get_all_notices_action(request)
 
-@router.get("/getCurrentNotices")
+@router.get("/api/getCurrentNotices")
 def get_current_notices(request: Request):
     return NoteController().get_current_notices_action(request)
 
-@router.post("/deleteNotice")
+@router.post("/api/deleteNotice")
 def delete_notice(notice: dict):
     return NoteController().delete_note_action(Notice, notice)
